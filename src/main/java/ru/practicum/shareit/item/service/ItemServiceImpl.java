dda223be81ed9dd72135ef6befa9891eb7028737
Marina Kosenko <ilovenovels@yandex.ru>
2023-04-29 15:31:21 +0300

    sprint13_v2

--- src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.item.service;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.practicum.shareit.handler.NotFoundException;
@@ -14,6 +15,7 @@ import ru.practicum.shareit.user.repository.UserRepository;
 import java.util.List;
 
 @Service
+@Slf4j
 public class ItemServiceImpl implements ItemService {
 
     private final ItemRepository itemRepository;
@@ -32,16 +34,19 @@ public class ItemServiceImpl implements ItemService {
         } catch (NotFoundException e) {
             throw new NotFoundUserForItemException("Такого пользователя в приложении нет!");
         }
+        log.debug("Добавляем товар " + itemDto.getName() + "...");
         return itemRepository.addItem(UserMapper.fromUserDto(userRepository.readUser(userId)), ItemMapper.fromItemDto(itemDto));
     }
 
     @Override
     public ItemDto editItem(Long ownerId, Long itemId, ItemDto itemDto) {
+        log.debug("Редактируем товар " + itemDto.getName() + "...");
         return itemRepository.editItem(ownerId, itemId, itemDto);
     }
 
     @Override
     public ItemDto getItem(Long itemId) {
+        log.debug("Получаем информацию о товаре с id " + itemId + "...");
         return itemRepository.getItem(itemId);
     }
 
