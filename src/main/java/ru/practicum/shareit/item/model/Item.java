442541c1c56cba048c7086b952f20b6dc0377737
Marina Kosenko <ilovenovels@yandex.ru>
2023-05-01 01:41:52 +0300

    sprint14_v1

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -2,16 +2,30 @@ package ru.practicum.shareit.item.model;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.Generated;
 import ru.practicum.shareit.request.ItemRequest;
 import ru.practicum.shareit.user.User;
 
+import javax.persistence.*;
+
 @Data
 @Builder
+@Table(name="items")
+@Entity
 public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private Long id;
+    @Column(name="name", nullable = false)
     private String name;
+    @Column(name="description", nullable = false)
     private String description;
+    @Column(name="items_available")
     private Boolean available;
+    @JoinColumn(name="owner_ids")
     private User owner;
+
+    //TODO перепроверить - нужно ли оставить JoinColumn или нужно прописать OneToMany или ManyToOne
     private ItemRequest request;
 }
--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -2,11 +2,23 @@ package ru.practicum.shareit.user;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.Generated;
+
+import javax.persistence.*;
 
 @Data
 @Builder
+@Table(name="users")
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private Long id;
+
+    @Column(name="name")
     private String name;
+
+    @Column(name="email")
     private String email;
 }
